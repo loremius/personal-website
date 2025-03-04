1a55406356d9a8fdb88c6c84ea10e337eda7edd2
Roman <romanbababa@outlook.com>
2025-03-04 12:03:47 +0300

    🐈‍⬛🐾

--- src/components/Providers.jsx
+++ src/components/Providers.jsx
@@ -1,11 +1,13 @@
 // @ts-check
 "use client";
 import ReactLenis from "@studio-freight/react-lenis";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Providers({ children }) {
 	return (
 		<ReactLenis root={true}>
 			{children}
+			<Analytics />
 		</ReactLenis>
 	);
 }
