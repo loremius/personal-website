914bfa9bf48a701a1bde1c1ab64445821aa14fc7
Roman <108236042+romanbaba@users.noreply.github.com>
2025-03-08 21:05:06 +0300

    ⬆️ Updated page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,19 +14,31 @@ import React from "react";
 
 export default function Page() {
 	const { data, isPending } = useSocial();
-	if (typeof isPending === "boolean" && isPending == true) return <SplashScreen />;
+	if (typeof isPending === "boolean" && isPending == true) {return <SplashScreen />;}
 
 	return (
 		<React.Fragment>
 			<Backdrop />
 			<Header />
-			<Popup title="v3.0.0 is here" description="Source code of the 3rd version of my personal website" iconName="sparkles" />
+			<Popup
+				title="v3.0.0 is here"
+				description="Source code of the 3rd version of my personal website"
+				iconName="sparkles"
+			/>
 
 			<div className="mx-auto w-full max-w-4xl lg:px-8 mt-20 relative">
-				<h2 className="text-4xl font-semibold">{data?.discord?.discord_user.global_name}</h2>
-				<h3 className="text-4xl font-semibold text-[#a1a1aa]">{config.badge}</h3>
-				<p className="text-[#a1a1aa] text-sm mt-2 max-w-2xl">{stringify(config.biography)}</p>
-				<p className="text-[#a1a1aa] text-sm mt-3 max-w-2xl">{stringify(config.description)}</p>
+				<h2 className="text-4xl font-semibold">
+					{data?.discord?.discord_user.global_name}
+				</h2>
+				<h3 className="text-4xl font-semibold text-[#a1a1aa]">
+					{config.badge}
+				</h3>
+				<p className="text-[#a1a1aa] text-sm mt-2 max-w-2xl">
+					{stringify(config.biography)}
+				</p>
+				<p className="text-[#a1a1aa] text-sm mt-3 max-w-2xl">
+					{stringify(config.description)}
+				</p>
 
 				<Activity />
 
